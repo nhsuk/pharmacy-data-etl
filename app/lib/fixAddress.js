3f43de523a666733cbafc910fa8e9659f6a96d2d
mark <markharrop@yahoo.com>
2017-06-13 10:48:09 +0100

    :house: fix duplicate address lines

---
+++ app/lib/fixAddress.js
@@ -0,0 +1,31 @@
+const fields = ['line1', 'line2', 'line3', 'city', 'county'];
+
+function getFieldName(fieldIndex) {
+  return fields[fieldIndex];
+}
+
+function toLowerCaseNoWhiteSpace(str) {
+  return str && str.toLowerCase().replace(/\s/g, '');
+}
+
+function areSame(a, b) {
+  return toLowerCaseNoWhiteSpace(a) === toLowerCaseNoWhiteSpace(b);
+}
+
+function blankDuplicateFieldNames(address, fieldIndex) {
+  for (let i = fieldIndex + 1; i <= fields.length - 1; i++) {
+    if (areSame(address[getFieldName(fieldIndex)], address[getFieldName(i)])) {
+      // eslint-disable-next-line no-param-reassign
+      address[getFieldName(i)] = '';
+    }
+  }
+}
+
+function fixAddress(address) {
+  for (let i = 0; i <= fields.length - 1; i++) {
+    blankDuplicateFieldNames(address, i);
+  }
+  return address;
+}
+
+module.exports = fixAddress;
---
+++ test/unit/fixAddress.js
@@ -0,0 +1,97 @@
+const chai = require('chai');
+const fixAddress = require('../../app/lib/fixAddress');
+
+const expect = chai.expect;
+
+
+describe('Fix Address', () => {
+  it('should blank duplicate line 1-2', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: '9 High Street',
+      line3: 'Walsall Wood',
+      city: 'Walsall',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.line1).to.equal('9 High Street');
+    expect(address.line2).to.equal('');
+  });
+
+  it('should blank duplicate line 2-3', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: 'Walsall Wood',
+      line3: 'Walsall Wood',
+      city: 'Walsall',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.line2).to.equal('Walsall Wood');
+    expect(address.line3).to.equal('');
+  });
+
+  it('should blank duplicate line 3-city', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: 'Walsall Wood',
+      line3: 'Walsall',
+      city: 'Walsall',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.line3).to.equal('Walsall');
+    expect(address.city).to.equal('');
+  });
+
+  it('should blank duplicate line city-county', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: 'Walsall Wood',
+      line3: 'Walsall',
+      city: 'West Midlands',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.city).to.equal('West Midlands');
+    expect(address.county).to.equal('');
+  });
+
+  it('should blank duplicate line, ignoring case', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: 'Walsall Wood',
+      line3: 'Walsall',
+      city: 'WALSALL',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.line3).to.equal('Walsall');
+    expect(address.city).to.equal('');
+  });
+
+  it('should blank duplicate line, ignoring whitespace', () => {
+    const address = {
+      line1: '9 High Street',
+      line2: 'Walsall Wood',
+      line3: 'WalsallWOOD',
+      city: '',
+      county: 'West Midlands',
+      postcode: 'WS9 9LR'
+    };
+
+    fixAddress(address);
+    expect(address.line2).to.equal('Walsall Wood');
+    expect(address.line3).to.equal('');
+  });
+});
